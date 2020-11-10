a17fa9043733d5db8c44fe3e39637428abf50d59
Eugene <eugenebobalo@gmail.com>
2020-11-09 20:27:05 -0600

    finished class 7 lab

--- Class5/Form Processing_with_jQuery/In_Class/soaps.js
+++ Class5/Form Processing_with_jQuery/In_Class/soaps.js
@@ -3,15 +3,36 @@ $(document).ready(function () {
     $("form").submit(placeOrder);
 
     function placeOrder(event) {
+        // prevent form from flashing and resetting
         event.preventDefault();
-        // find all the checked 
+
         var subtotal = 0;
+        var soapNames = "";
 
+        // find all the checked boxes
+        var checkedBoxes = $("input[name=soap]:checked");
+
+        // add up their data-price values
         checkedBoxes.each(function () {
+            // add each box's data-price value to the subtotal
             subtotal += $(this).data("price");
-        })
+            soapNames += $(this).val();
+            soapNames += "<br>";
+            //   var discount = $(this).data("discount");
+        });
 
+        var selectedRadioButton = $("input[name=shipping]:checked");
+
+        var shippingCost = selectedRadioButton.data("shipping-fee");
+
+        var orderTotal = subtotal + shippingCost;
+
+        // put total onscreen
         $("#soapOutput").text(subtotal.toFixed(2));
+        $("#shippingOutput").text(shippingCost.toFixed(2));
+        $("#totalOutput").text(orderTotal.toFixed(2));
+        // Don't output the tags! Render them as breaks
+        $("#soaps").html(soapNames);
     }
 
     function updateButton() {
@@ -20,6 +41,5 @@ $(document).ready(function () {
         var numSelections = checkedBoxes.length;
 
         $("#quantity").text(numSelections);
-
     }
-})
+});
---
+++ Unit_2/javascript-trip-calculator-master/tripCalc.js
@@ -0,0 +1,67 @@
+// The line below ensures that no JavaScript will run
+// until the document (the web page) is completely
+// loaded and the DOM is ready to access. We put all
+// our JavaScript code and functions inside the curly
+// braces of the ready function.
+$(document).ready(function () {
+    // event handlers
+    $("button").click(calculateTrip);
+
+    function calculateTrip() {
+        /*
+         GATHER INPUT FROM WEB PAGE
+         */
+        // Get the value from the input box with id="miles"
+        var milesDriven = $("#miles").val();
+
+        // Convert to a number data type (including decimals)
+        milesDriven = parseFloat(milesDriven);
+
+        // Get the value from the input box with id="mpg"
+        var milesPerGallon = $("#mpg").val();
+
+        // Convert to a number data type (including decimals)
+        milesPerGallon = parseFloat(milesPerGallon);
+
+        // Get the value from the input box with id="mph"
+        var milesPerHour = $("#mph").val();
+
+        // Convert to a number data type (truncating decimals)
+        milesPerHour = parseInt(milesPerHour);
+
+        // Get the value from the input box with id="dpg"
+        var dollarsPerGallon = $("#dpg").val();
+
+        // Convert to a number data type (including decimals)
+        dollarsPerGallon = parseFloat(dollarsPerGallon);
+
+        /*
+        PERFORM CALCULATIONS AND DISPLAY OUTPUT
+         */
+
+        // Calculate the total hours for the trip
+        var hours = milesDriven / milesPerHour;
+
+        // Round the hours to one decimal place for display
+        var hoursDisplay = hours.toFixed(1);
+
+        // Update the text in the span with id="tripHours"
+        $("#tripHours").text(hoursDisplay);
+
+        // Calculate the total gas used
+        var totalGallonsUsed = milesDriven / milesPerGallon;
+
+        // Calculate the cost for that many gallons
+        var costOfGas = totalGallonsUsed * dollarsPerGallon;
+
+        // Round the cost to dollars/cents for display
+        var costOfGasDisplay = costOfGas.toFixed(2);
+
+        // Update the text in the span with id="tripCost"
+        $("#tripCost").text(costOfGasDisplay);
+
+        // Unhide the results div
+        $(".output").show();
+    }
+
+});
