d59ec370f4997987f6bcd4a10f81ccfd2a639e13
Eugene <eugenebobalo@gmail.com>
2020-11-17 21:14:53 -0600

    Unit2 Project stuff

---
+++ Class9/script.js
@@ -0,0 +1,17 @@
+$(document).ready(
+    function () {
+        // event handlers
+
+        // $("input").changed(showOrder);
+
+    });
+
+function runFunction(event) {
+    var numWineGlassesPrinted = 0;
+    var wineDrank = 3;
+
+
+
+}
+
+
